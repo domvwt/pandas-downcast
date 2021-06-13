aa7678e9e2a6b0a0244b1d7405fb8be5360f5b7a
Dom <dominic.thorn@gmail.com>
2021-06-13 13:17:30 +0100

    Develop (#2)
    * Update library import
    
    * Update dependencies
    
    * Update Readme
    
    * Update version
    
    * Add codecov.yml

--- pdcast/__init__.py
+++ pdcast/__init__.py
@@ -5,7 +5,7 @@
 Safely infer minimum viable schema for Pandas DataFrames and Series.
 
 Examples:
-    import pdcast as pc
+    import pdcast as pdc
 
     import numpy as np
     import pandas as pd
@@ -20,17 +20,17 @@ Examples:
     df = pd.DataFrame(data)
 
     # Downcast DataFrame to minimum viable schema.
-    df_downcast = pc.downcast(df)
+    df_downcast = pdc.downcast(df)
 
     # Infer minimum schema from DataFrame.
-    schema = pc.infer_schema(df)
+    schema = pdc.infer_schema(df)
 
     # Coerce DataFrame to schema - required if converting float to Pandas Integer.
-    df_new = pc.coerce_df(df)
+    df_new = pdc.coerce_df(df)
 """
 
 __author__ = """Dominic Thorn"""
 __email__ = "dominic.thorn@gmail.com"
-__version__ = "0.1.0.dev1"
+__version__ = "0.1.0"
 
 from pdcast.core import coerce_df, coerce_series, downcast, infer_schema, options
