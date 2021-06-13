89897fd15ff9ba83c7d73e7f992630a8e42a6541
Dominic Thorn <dominic.thorn@gmail.com>
2021-06-13 13:13:00 +0100

    Update version

--- pdcast/__init__.py
+++ pdcast/__init__.py
@@ -31,6 +31,6 @@ Examples:
 
 __author__ = """Dominic Thorn"""
 __email__ = "dominic.thorn@gmail.com"
-__version__ = "0.1.0.dev1"
+__version__ = "0.1.0"
 
 from pdcast.core import coerce_df, coerce_series, downcast, infer_schema, options
